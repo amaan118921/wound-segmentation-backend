2213a979af33cdf06a6249138b005224473088ea
Amaan Ur Rahman <amaanurrahman00@gmail.com>
2023-10-30 21:48:09 +0530

    changess

--- app.py
+++ app.py
@@ -11,8 +11,9 @@ def hello_world():
 
 @app.route('/predict')
 def predict():
-    url = request.args.get('url')
-    filename = request.args.get('filename')
-    name = filename + '.jpg'
+    # url = request.args.get('url')
+    # filename = request.args.get('filename')
+    url = 'https://firebasestorage.googleapis.com/v0/b/womensafety-c4d41.appspot.com/o/foot-ulcer-0027.png?alt=media&token=01577279-05f7-48b3-8e8d-52e318ea3cec'
+    name = "resultttt.jpg"
     if pr.predict_result(url, name): return 'success'
     return 'failed'
